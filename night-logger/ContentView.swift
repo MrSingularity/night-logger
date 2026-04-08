4158cb94c8e1b2c573af4caa4ed136aac9bb9b13
Gabriel Gillmann <gabriel.gillmann@bluewin.ch>
2026-04-08 15:37:42 +0200

    Initial Commit

---
+++ night-logger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  night-logger
+//
+//  Created by Gabriel Gillmann on 08.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ night-logger/night_loggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  night_loggerApp.swift
+//  night-logger
+//
+//  Created by Gabriel Gillmann on 08.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct night_loggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
